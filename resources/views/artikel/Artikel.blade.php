863d703a5d0a60bfbbf11bc30c78b2b8127d8834
achmadrafi777 <achmadrafirasyiddin@student.telkomuniversity.ac.id>
2022-04-16 16:25:58 +0700

    Artikel.blade improvement (add container)

--- resources/views/artikel/Artikel.blade.php
+++ resources/views/artikel/Artikel.blade.php
@@ -10,128 +10,128 @@
 <br>
 <br>
 
-<div class="container">
-<h5><b>Jurusan Rekomendasi Kami</b></h5>
-<br>
-<br>
+<div style="border-radius: 100px;" class="container">
 
-<div class="container">
-    <div class="card">
-        <div class="mx-auto">
-            <div class="row row-cols-3 row-cols-3 g-4">
+    <div class="shadow p-3 mb-5 bg-body rounded">
+    <h5><b>Jurusan Rekomendasi Kami</b></h5>
+    <br>
+    <br>
 
-                <div class="col">
-                    <div class="card">
-                    <img src="https://binus.ac.id/wp-content/uploads/2020/10/8-0-Yuk-Kuliah-Sambil-Keliling-Dunia-Bersama-Jurusan-Hubungan-Internasional.png" class="card-img-top" alt="...">
-                    <div class="card-body">
-                        <center><h7 class="card-title"><b>Hubungan Internasional</b></center>
-                        <p class="card-text">Kepo Jurusan akan membawa pembahasan seputar pengenalan dunia perkuliahan mulai dari mata kuliah apa saja yang dipelajari, skill apa saja yang di kembangkan, prospek karir, dll . So tunggu apalagii yuk buruan daftarkan diri dengan klik tombol daftar dibawah karena slot peserta snagat terbatas </p></h7>
-                    </div>
-                    <div class="card-footer">
-                        <center><button type="button" href="#" class="btn btn-rounded btn-secondary">See Details</button></center>
-                    </div>
-                    </div>
-                </div>
+        <div class="container">
+            <div class="card">
+                <div class="mx-auto">
+                    <div class="row row-cols-3 row-cols-3 g-4">
 
-                <div class="col">
-                    <div class="card">
-                    <img src="https://binus.ac.id/wp-content/uploads/2020/10/8-0-Yuk-Kuliah-Sambil-Keliling-Dunia-Bersama-Jurusan-Hubungan-Internasional.png" class="card-img-top" alt="...">
-                    <div class="card-body">
-                        <center><h7 class="card-title"><b>Sistem Informasi</b></center>
-                        <p class="card-text">Kepo Jurusan akan membawa pembahasan seputar pengenalan dunia perkuliahan mulai dari mata kuliah apa saja yang dipelajari, skill apa saja yang di kembangkan, prospek karir, dll . So tunggu apalagii yuk buruan daftarkan diri dengan klik tombol daftar dibawah karena slot peserta snagat terbatas </p></h7>
-                    </div>
-                    <div class="card-footer">
-                        <center><button type="button" href="#" class="btn btn-rounded btn-secondary">See Details</button></center>
-                    </div>
-                    </div>
-                </div>
+                        <div class="col">
+                            <div class="card">
+                            <img src="https://binus.ac.id/wp-content/uploads/2020/10/8-0-Yuk-Kuliah-Sambil-Keliling-Dunia-Bersama-Jurusan-Hubungan-Internasional.png" class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <center><h7 class="card-title"><b>Hubungan Internasional</b></center>
+                                <p class="card-text">Kepo Jurusan akan membawa pembahasan seputar pengenalan dunia perkuliahan mulai dari mata kuliah apa saja yang dipelajari, skill apa saja yang di kembangkan, prospek karir, dll . So tunggu apalagii yuk buruan daftarkan diri dengan klik tombol daftar dibawah karena slot peserta snagat terbatas </p></h7>
+                            <div class="card-footer">
+                                <center><button type="button" href="#" class="btn btn-rounded btn-secondary">See Details</button></center>
+                            </div>
+                            </div>
+                            </div>
+                        </div>
 
-                <div class="col">
-                    <div class="card">
-                    <img src="https://binus.ac.id/wp-content/uploads/2020/10/8-0-Yuk-Kuliah-Sambil-Keliling-Dunia-Bersama-Jurusan-Hubungan-Internasional.png" class="card-img-top" alt="...">
-                    <div class="card-body">
-                        <center><h7 class="card-title"><b>Teknik Informatika</b></center>
-                        <p class="card-text">Kepo Jurusan akan membawa pembahasan seputar pengenalan dunia perkuliahan mulai dari mata kuliah apa saja yang dipelajari, skill apa saja yang di kembangkan, prospek karir, dll . So tunggu apalagii yuk buruan daftarkan diri dengan klik tombol daftar dibawah karena slot peserta snagat terbatas </p></h7>
-                    </div>
-                    <div class="card-footer">
-                        <center><button type="button" href="#" class="btn btn-rounded btn-secondary">See Details</button></center> 
-                    </div>
-                    </div>
-                    <br>
-                    <div class="footer">
-                        <center><button type="button" href="#" class="btn light btn-light">Lihat Lainnya ></button></center>
-                    <br>
-                </div>
-            </div>
-        </div>
-     </div>
-</div>
-<br>
-<br>
-<br>
-<br>
-<br>
+                        <div class="col">
+                            <div class="card">
+                            <img src="https://binus.ac.id/wp-content/uploads/2020/10/8-0-Yuk-Kuliah-Sambil-Keliling-Dunia-Bersama-Jurusan-Hubungan-Internasional.png" class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <center><h7 class="card-title"><b>Sistem Informasi</b></center>
+                                <p class="card-text">Kepo Jurusan akan membawa pembahasan seputar pengenalan dunia perkuliahan mulai dari mata kuliah apa saja yang dipelajari, skill apa saja yang di kembangkan, prospek karir, dll . So tunggu apalagii yuk buruan daftarkan diri dengan klik tombol daftar dibawah karena slot peserta snagat terbatas </p></h7>
+                            </div>
+                            <div class="card-footer">
+                                <center><button type="button" href="#" class="btn btn-rounded btn-secondary">See Details</button></center>
+                            </div>
+                            </div>
+                        </div>
 
-<div class="container">
-<h5><b>Kampus Rekomendasi Kami</b></h5>
-<br>
-<br>
-
-<div class="container">
-    <div class="card">
-        <div class="mx-auto">
-            <div class="row row-cols-3 row-cols-3 g-4">
-
-
-                <div class="col">
-                    <div class="card">
-                    <img src="https://binus.ac.id/wp-content/uploads/2020/10/8-0-Yuk-Kuliah-Sambil-Keliling-Dunia-Bersama-Jurusan-Hubungan-Internasional.png" class="card-img-top" alt="...">
-                    <div class="card-body">
-                        <center><h7 class="card-title"><b>Universitas Telkom</b></center>
-                        <p class="card-text">Kepo Jurusan akan membawa pembahasan seputar pengenalan dunia perkuliahan mulai dari mata kuliah apa saja yang dipelajari, skill apa saja yang di kembangkan, prospek karir, dll . So tunggu apalagii yuk buruan daftarkan diri dengan klik tombol daftar dibawah karena slot peserta snagat terbatas </p></h7>
+                        <div class="col">
+                            <div class="card">
+                            <img src="https://binus.ac.id/wp-content/uploads/2020/10/8-0-Yuk-Kuliah-Sambil-Keliling-Dunia-Bersama-Jurusan-Hubungan-Internasional.png" class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <center><h7 class="card-title"><b>Teknik Informatika</b></center>
+                                <p class="card-text">Kepo Jurusan akan membawa pembahasan seputar pengenalan dunia perkuliahan mulai dari mata kuliah apa saja yang dipelajari, skill apa saja yang di kembangkan, prospek karir, dll . So tunggu apalagii yuk buruan daftarkan diri dengan klik tombol daftar dibawah karena slot peserta snagat terbatas </p></h7>
+                            </div>
+                            <div class="card-footer">
+                                <center><button type="button" href="#" class="btn btn-rounded btn-secondary">See Details</button></center> 
+                            </div>
+                            </div>
+                            <br>
+                            <div class="footer">
+                                <center><button type="button" href="/artikel/jurusan" class="btn light btn-light">Lihat Lainnya ></button></center>
+                            <br>
+                        </div>
                     </div>
-                    <div class="card-footer">
-                        <center><button type="button" href="#" class="btn btn-rounded btn-secondary">See Details</button></center>
-                    </div>
-                    </div>
-                </div>
-
-                <div class="col">
-                    <div class="card">
-                    <img src="https://binus.ac.id/wp-content/uploads/2020/10/8-0-Yuk-Kuliah-Sambil-Keliling-Dunia-Bersama-Jurusan-Hubungan-Internasional.png" class="card-img-top" alt="...">
-                    <div class="card-body">
-                        <center><h7 class="card-title"><b>Institut Teknologi Bandung</b></center>
-                        <p class="card-text">Kepo Jurusan akan membawa pembahasan seputar pengenalan dunia perkuliahan mulai dari mata kuliah apa saja yang dipelajari, skill apa saja yang di kembangkan, prospek karir, dll . So tunggu apalagii yuk buruan daftarkan diri dengan klik tombol daftar dibawah karena slot peserta snagat terbatas </p></h7>
-                    </div>
-                    <div class="card-footer">
-                        <center><button type="button" href="#" class="btn btn-rounded btn-secondary">See Details</button></center>
-                    </div>
-                    </div>
-                </div>
-
-                <div class="col">
-                    <div class="card">
-                    <img src="https://binus.ac.id/wp-content/uploads/2020/10/8-0-Yuk-Kuliah-Sambil-Keliling-Dunia-Bersama-Jurusan-Hubungan-Internasional.png" class="card-img-top" alt="...">
-                    <div class="card-body">
-                        <center><h7 class="card-title"><b>Universitas Padjajaran</b></center>
-                        <p class="card-text">Kepo Jurusan akan membawa pembahasan seputar pengenalan dunia perkuliahan mulai dari mata kuliah apa saja yang dipelajari, skill apa saja yang di kembangkan, prospek karir, dll . So tunggu apalagii yuk buruan daftarkan diri dengan klik tombol daftar dibawah karena slot peserta snagat terbatas </p></h7>
-                    </div>
-                    <div class="card-footer">
-                        <center><button type="button" href="#" class="btn btn-rounded btn-secondary">See Details</button></center>
-                    </div>
-                    </div>
-                    <br>
-                    <div class="footer">
-                        <center><button type="button" href="#" class="btn light btn-light">Lihat Lainnya ></button></center>
-                    <br>
-                    
                 </div>
             </div>
         </div>
-     </div>
-</div>
-<br>
-<br>
-<br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+
+
+
+        <div class="container">
+        <h5><b>Kampus Rekomendasi Kami</b></h5>
+        <br>
+        <br>
+
+        <div class="container">
+            <div class="card">
+                <div class="mx-auto">
+                    <div class="row row-cols-3 row-cols-3 g-4">
+
+
+                        <div class="col">
+                            <div class="card">
+                            <img src="https://binus.ac.id/wp-content/uploads/2020/10/8-0-Yuk-Kuliah-Sambil-Keliling-Dunia-Bersama-Jurusan-Hubungan-Internasional.png" class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <center><h7 class="card-title"><b>Universitas Telkom</b></center>
+                                <p class="card-text">Kepo Jurusan akan membawa pembahasan seputar pengenalan dunia perkuliahan mulai dari mata kuliah apa saja yang dipelajari, skill apa saja yang di kembangkan, prospek karir, dll . So tunggu apalagii yuk buruan daftarkan diri dengan klik tombol daftar dibawah karena slot peserta snagat terbatas </p></h7>
+                            </div>
+                            <div class="card-footer">
+                                <center><button type="button" href="#" class="btn btn-rounded btn-secondary">See Details</button></center>
+                            </div>
+                            </div>
+                        </div>
+
+                        <div class="col">
+                            <div class="card">
+                            <img src="https://binus.ac.id/wp-content/uploads/2020/10/8-0-Yuk-Kuliah-Sambil-Keliling-Dunia-Bersama-Jurusan-Hubungan-Internasional.png" class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <center><h7 class="card-title"><b>Institut Teknologi Bandung</b></center>
+                                <p class="card-text">Kepo Jurusan akan membawa pembahasan seputar pengenalan dunia perkuliahan mulai dari mata kuliah apa saja yang dipelajari, skill apa saja yang di kembangkan, prospek karir, dll . So tunggu apalagii yuk buruan daftarkan diri dengan klik tombol daftar dibawah karena slot peserta snagat terbatas </p></h7>
+                            </div>
+                            <div class="card-footer">
+                                <center><button type="button" href="#" class="btn btn-rounded btn-secondary">See Details</button></center>
+                            </div>
+                            </div>
+                        </div>
+
+                        <div class="col">
+                            <div class="card">
+                            <img src="https://binus.ac.id/wp-content/uploads/2020/10/8-0-Yuk-Kuliah-Sambil-Keliling-Dunia-Bersama-Jurusan-Hubungan-Internasional.png" class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <center><h7 class="card-title"><b>Universitas Padjajaran</b></center>
+                                <p class="card-text">Kepo Jurusan akan membawa pembahasan seputar pengenalan dunia perkuliahan mulai dari mata kuliah apa saja yang dipelajari, skill apa saja yang di kembangkan, prospek karir, dll . So tunggu apalagii yuk buruan daftarkan diri dengan klik tombol daftar dibawah karena slot peserta snagat terbatas </p></h7>
+                            </div>
+                            <div class="card-footer">
+                                <center><button type="button" href="#" class="btn btn-rounded btn-secondary">See Details</button></center>
+                            </div>
+                            </div>
+                            <br>
+                            <div class="footer">
+                                <center><a type="button" href="/artikel/kampus" class="btn light btn-light">Lihat Lainnya ></button></center>
+                            <br> 
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
 
 @endsection
