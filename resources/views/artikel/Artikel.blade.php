a7875f42f847523924733a138595934c8dfb1ca0
achmadrafi777 <achmadrafirasyiddin@student.telkomuniversity.ac.id>
2022-04-18 05:14:30 +0700

    Menu awal artikel, artikel jurusan, artikel kampus

---
+++ resources/views/artikel/Artikel.blade.php
@@ -0,0 +1,138 @@
+@extends('layouts.app')
+
+@section('content')
+
+
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+
+<div style="border-radius: 100px;" class="container">
+
+    <div class="shadow p-3 mb-5 bg-body rounded">
+    <h5><b>Jurusan Rekomendasi Kami</b></h5>
+    <br>
+
+        <div class="container">
+            <div class="card">
+                <div class="mx-auto">
+                    <div class="row row-cols-3 row-cols-3 g-4">
+
+                        <div class="col">
+                            <div class="card">
+                            <img src="https://binus.ac.id/wp-content/uploads/2020/10/8-0-Yuk-Kuliah-Sambil-Keliling-Dunia-Bersama-Jurusan-Hubungan-Internasional.png" class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <center><h7 class="card-title"><b>Hubungan Internasional</b></center>
+                                <center><p class="card-text">Hubungan Internasional berkaitan erat dengan upaya suatu negara menghadapi tantangan internasional seperti ketidakseimbangan perdagangan dunia, isu keamanan, masalah lingkungan secara global, dan kemiskinan melalui kerja sama antarnegara. Kuliah di jurusan ini memungkinkanmu untuk merasakan atmosfer internasional melalui berbagai simulasi pertemuan-pertemuan penting di dunia, salah satunya sidang PBB. Seru kan Quipperian? Hubungan antarnegara aja dijaga, apalagi hubungan kita?</p></h7></center>
+                            <div class="card-footer">
+                                <center><button type="button" href="#" class="btn btn-rounded btn-secondary">See Details</button></center>
+                            </div>
+                            </div>
+                            </div>
+                        </div>
+
+                        <div class="col">
+                            <div class="card">
+                            <img src="https://binus.ac.id/wp-content/uploads/2020/10/8-0-Yuk-Kuliah-Sambil-Keliling-Dunia-Bersama-Jurusan-Hubungan-Internasional.png" class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <center><h7 class="card-title"><b>Sistem Informasi</b></center>
+                                <center><p class="card-text">Jurusan Sistem Informasi adalah bidang keilmuan yang menggabungkan ilmu komputer dengan bisnis dan manajemen. Di prodi ini kamu akan belajar gimana mengidentifikasi kebutuhan dan proses bisnis perusahaan berdasarkan data-data yang dimiliki perusahan, kemudian merancang sistem yang sesuai dengan kebutuhan perusahaan.
+Jadi, selain belajar teknik pemrograman, kamu juga dituntut untuk mempelajari proses bisnis yang ada di perusahaan.</p></h7></center>
+                            </div>
+                            <div class="card-footer">
+                                <center><button type="button" href="#" class="btn btn-rounded btn-secondary">See Details</button></center>
+                            </div>
+                            </div>
+                        </div>
+
+                        <div class="col">
+                            <div class="card">
+                            <img src="https://binus.ac.id/wp-content/uploads/2020/10/8-0-Yuk-Kuliah-Sambil-Keliling-Dunia-Bersama-Jurusan-Hubungan-Internasional.png" class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <center><h7 class="card-title"><b>Teknik Informatika</b></center>
+                                <center><p class="card-text">Teknik Informatika merupakan bidang ilmu yang mempelajari bagaimana menggunakan teknologi komputer secara optimal guna menangani masalah transformasi atau pengolahan data dengan proses logika. Di Jurusan Teknik Informatika kamu akan mempelajari berbagai prinsip terkait ilmu komputer mulai dari proses perancangan, pengembangan, pengujian, hingga evaluasi sistem operasi perangkat lunak. Selama kuliah kamu akan banyak mengkaji pemrograman dan komputasi, dan dibekali pula dengan keterampilan merancang perangkat lunak. </p></h7></center>
+                            </div>
+                            <div class="card-footer">
+                                <center><button type="button" href="#" class="btn btn-rounded btn-secondary">See Details</button></center> 
+                            </div>
+                            </div>
+                        
+                            <br>
+                            <div class="footer">
+                                <center><button type="button" href="/artikel/jurusan" class="btn light btn-light">Lihat Lainnya ></button></center>
+                            <br>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<br>
+<br>
+
+<div style="border-radius: 100px;" class="container">
+
+    <div class="shadow p-3 mb-5 bg-body rounded">
+    <h5><b>Kampus Rekomendasi Kami</b></h5>
+    <br>
+
+        <div class="container">
+            <div class="card">
+                <div class="mx-auto">
+                    <div class="row row-cols-3 row-cols-3 g-4">
+
+
+                        <div class="col">
+                            <div class="card">
+                            <img src="https://binus.ac.id/wp-content/uploads/2020/10/8-0-Yuk-Kuliah-Sambil-Keliling-Dunia-Bersama-Jurusan-Hubungan-Internasional.png" class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <center><h7 class="card-title"><b>Institut Teknologi Bandung</b></center>
+                                <center><p class="card-text">Institut Teknologi Bandung merupakan kampus impian bagi hampir semua pelajar di Indonesia, terutama yang ingin melanjutkan pendidikan pada bidang teknik/sains. Bagaimana tidak, kampus ini merupakan lokasi dari sekolah tinggi teknik pertama di Indonesia yang berdiri pada tahun 1920 dengan nama De Technische Hoogeschool te Bandung. Institut Teknologi Bandung telah lebih dari 50 tahun berkontribusi dalam menyelenggarakan pendidikan tinggi di Indonesia. Dan banyak banget lho tokoh-tokoh besar negeri ini yang merupakan alumni dari kampus ini! </p></h7></center>
+                            </div>
+                            <div class="card-footer">
+                                <center><button type="button" href="#" class="btn btn-rounded btn-secondary">See Details</button></center>
+                            </div>
+                            </div>
+                        </div>
+
+                        <div class="col">
+                            <div class="card">
+                            <img src="https://binus.ac.id/wp-content/uploads/2020/10/8-0-Yuk-Kuliah-Sambil-Keliling-Dunia-Bersama-Jurusan-Hubungan-Internasional.png" class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <center><h7 class="card-title"><b>Universitas Telkom</b></center>
+                                <center><p class="card-text">Telkom University berdiri pada tanggal 14 Agustus 2013 berdasarkan Surat Keputusan Direktur Jenderal Pendidikan Tinggi Kemendikbud Nomor 309/E/0/2013. Telkom University adalah Perguruan Tinggi Swasta yang diselenggarakan oleh Yayasan Pendidikan Telkom, dan merupakan penggabungan dari empat Perguruan Tinggi Swasta, yaitu institut Teknologi Telkom (IT Telkom), Institut Manajemen Telkom (IM Telkom), Politeknik Telkom, dan Sekolah Tinggi Seni Rupa dan Desain Indonesia Telkom (STISI Telkom). </p></h7></center>
+                            </div>
+                            <div class="card-footer">
+                                <center><button type="button" href="#" class="btn btn-rounded btn-secondary">See Details</button></center>
+                            </div>
+                            </div>
+                        </div>
+
+                        <div class="col">
+                            <div class="card">
+                            <img src="https://binus.ac.id/wp-content/uploads/2020/10/8-0-Yuk-Kuliah-Sambil-Keliling-Dunia-Bersama-Jurusan-Hubungan-Internasional.png" class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <center><h7 class="card-title"><b>Universitas Padjajaran</b></center>
+                                <center><p class="card-text"> tahukah kamu bahwa Universitas Padjadjaran berhasil menjadi universitas terfavorit di Indonesia selama bertahun-tahun? Sebagai salah satu universitas terbaik di Indonesia, Universitas Padjadjaran berhasil menarik minat siswa di Indonesia untuk bisa menempuh pendidikan di kampus Bumi Parahyangan ini. Tentu akan menjadi kebanggaan tersendiri bagi Quipperian yang berhasil bersaing dengan puluhan ribu siswa dan mendapatkan kursi untuk belajar di kampus yang memiliki jumlah mahasiswa asing terbanyak kedua di Indonesia ini. </p></h7></center>
+                            </div>
+                            <div class="card-footer">
+                                <center><button type="button" href="#" class="btn btn-rounded btn-secondary">See Details</button></center>
+                            </div>
+                            </div>
+                            <br>
+                            <div class="footer">
+                                <center><a type="button" href="/artikel/kampus" class="btn light btn-light">Lihat Lainnya ></button></center> 
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+@endsection
